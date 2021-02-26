fb15dacd9b0563f7199f6d63c66b5290b01a1928
cuong.le <lequoccuong059@gmail.com>
2021-02-26 15:22:19 +0700

    init source

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import { useStore } from "../redux/store";
+
+export default function App({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState);
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
---
+++ redux/reducers/counterReducer.js
@@ -0,0 +1,16 @@
+import { combineReducers } from "redux";
+import * as types from "../types/types";
+
+// COUNTER REDUCER
+export const counterReducer = (state = 0, { type }) => {
+  switch (type) {
+    case types.INCREMENT:
+      return state + 1;
+    case types.DECREMENT:
+      return state - 1;
+    case types.RESET:
+      return 0;
+    default:
+      return state;
+  }
+};
